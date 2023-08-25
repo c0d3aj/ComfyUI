f7d0adda93b5b814549f38440dbcdf791ecefd3e
Adrian Jastrzebski <a.jastrzebski@quantdevlabs.com>
2023-08-25 19:43:12 +0200

    update dockerfile

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login registry.quantdevlabs.com
-docker build -t registry.quantdevlabs.com/machine-learning/cgc-comfy-ui -f dev.Dockerfile .
+docker build -t registry.quantdevlabs.com/machine-learning/cgc-comfy-ui -f Dockerfile .
 docker push registry.quantdevlabs.com/machine-learning/cgc-comfy-ui
